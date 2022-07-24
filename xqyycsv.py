916dd5326aa8d4d810afac8bb1d70beb9bafcb04
waltergas <76164562+waltergas@users.noreply.github.com>
2022-07-24 17:27:21 +0800

    Update xqyycsv.py

--- xqyycsv.py
+++ xqyycsv.py
@@ -4,7 +4,7 @@ import datetime
 import csv
 header={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}#模仿浏览器
 url1 = 'https://healthcare.xqyk024.com/consult/diagnosis/doctors?pageNumber=1&pageSize=50&order=0'
-response = requests(url1)
+response = requests(url1, headers=header)
 response.encoding = 'utf-8'
 html = response.text
 # html = buff.decode()
